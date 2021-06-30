45aa10840d277f73df1885ba933915771ae9ea4c
Jonny Rodriguez <jojoroto@gmail.com>
2021-06-29 20:42:51 -0400

    Update proxyserver.py

--- proxyserver.py
+++ proxyserver.py
@@ -4,6 +4,7 @@ from os import environ
 import pymongo
 import pickle
 import json
+import sign
 import time
 
 
@@ -24,14 +25,23 @@ async def read_body(receive):
 
 
 async def app(scope, receive, send):
-    data = json.loads(await read_body(receive))
-    print(data)
+    headers = dict(scope['headers'])
+    body = await read_body(receive)
+    signature = headers.get(b'signature', b'')
+
+    if not sign.verify(body, signature):
+        return await errorresponse(send, 'Unauthorized')
+
+    data = json.loads(body)
 
     obj = mongo[data.get('db')][data.get('coll')]
     func = getattr(obj, data.get('func'))
-
     s = time.time()
-    result = func(*data.get('args'), **data.get('kwargs'))
+
+    try:
+        result = func(*data.get('args'), **data.get('kwargs'))
+    except Exception as e:
+        return await errorresponse(send, str(e))
 
     await send({
         'type': 'http.response.start',
@@ -49,3 +59,18 @@ async def app(scope, receive, send):
         'body': pickle.dumps(result)
     })
     print(time.time()-s)
+
+
+async def errorresponse(send, msg):
+    await send({
+        'type': 'http.response.start',
+        'status': 400,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': msg.encode('utf-8')
+    })
+    return None
