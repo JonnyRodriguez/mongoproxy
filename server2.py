1ad4dd6edde5802f35ea97e585119d19363fd864
Jonny Rodriguez <jojoroto@gmail.com>
2021-07-04 18:05:59 -0400

    Update server2.py

--- server2.py
+++ server2.py
@@ -4,3 +4,6 @@ from mongoproxy.server import *
 
 load_dotenv()
 mongoinit(environ.get('mongouri'))
+
+if __name__ == "__main__":
+  uvicorn.run("server2:app", host="0.0.0.0", port=5000, log_level="info")
